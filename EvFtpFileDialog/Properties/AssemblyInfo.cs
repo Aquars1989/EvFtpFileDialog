646d42717274acb6e700d2adc44d445d90d965ce
Evan.Hung <Evan.hung@dynapack.com.tw>
2023-12-28 16:01:07 +0800

    add assembly information

--- EvFtpFileDialog/Properties/AssemblyInfo.cs
+++ EvFtpFileDialog/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EvFtpFileDialog")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provid a dialog box to select file on ftp")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Evan")]
 [assembly: AssemblyProduct("EvFtpFileDialog")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
