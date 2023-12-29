ded1e62864326d00f77f7d80d9e63214dfc3eb55
Evan.Hung <Evan.hung@dynapack.com.tw>
2023-12-29 16:36:17 +0800

    Add a loading box, fix path with empty root

---
+++ EvFtpFileDialog.Test/TestInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EvFtpFileDialog.Test
+{
+    class TestInfo
+    {
+        public static string Address = "ftp.dlptest.com";
+        public static string StartupPath = "/";
+        public static string UserName = "dlpuser";
+        public static string Password = "rNrKYTX9g7z3RgJRmxWuGHbeu";
+    }
+}
